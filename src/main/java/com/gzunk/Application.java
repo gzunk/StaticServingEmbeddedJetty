7885494ce55e4b9224445d7c45b3af32dfb19c09
Alasdair Thomson <alasdair@gzunk.com>
2017-08-28 22:38:40 +0100

    New method to serve static content, and initial Angular experiments

--- src/main/java/com/gzunk/Application.java
+++ src/main/java/com/gzunk/Application.java
@@ -19,31 +19,31 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    @Bean
-    public ServletRegistrationBean servletRegistrationBean(){
+//    @Bean
+//    public ServletRegistrationBean servletRegistrationBean(){
+//
+//        ServletRegistrationBean srb = new ServletRegistrationBean(new DefaultServlet(), "/hello/*");
+//        String homePath = System.getProperty("user.home");
+//        srb.addInitParameter("resourceBase", homePath);
+//        srb.addInitParameter("dirAllowed","true");
+//        srb.addInitParameter("pathInfoOnly","true");
+//
+//        return srb;
+//    }
 
-        ServletRegistrationBean srb = new ServletRegistrationBean(new DefaultServlet(), "/hello/*");
-        String homePath = System.getProperty("user.home");
-        srb.addInitParameter("resourceBase", homePath);
-        srb.addInitParameter("dirAllowed","true");
-        srb.addInitParameter("pathInfoOnly","true");
-
-        return srb;
-    }
-
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-
-        };
-    }
+//    @Bean
+//    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+//        return args -> {
+//
+//            System.out.println("Let's inspect the beans provided by Spring Boot:");
+//
+//            String[] beanNames = ctx.getBeanDefinitionNames();
+//            Arrays.sort(beanNames);
+//            for (String beanName : beanNames) {
+//                System.out.println(beanName);
+//            }
+//
+//        };
+//    }
 
 }
--- src/main/java/com/gzunk/HelloController.java
+++ src/main/java/com/gzunk/HelloController.java
@@ -1,14 +1,30 @@
 package com.gzunk;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.annotation.Resource;
+
 @RestController
 public class HelloController {
-    
-    @RequestMapping("/")
+
+    @Resource
+    private ApplicationContext applicationContext;
+
+    @RequestMapping("/greeting")
     public String index() {
         return "Greetings from Spring Boot!";
     }
-    
+
+    @RequestMapping("/close")
+    @ResponseStatus(value = HttpStatus.OK)
+    public void close() {
+        SpringApplication.exit(applicationContext);
+    }
+
+
 }
